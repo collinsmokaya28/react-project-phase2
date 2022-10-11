8573c787807be9379d5d46783ab0ea934ab02b7a
Collins Mokaya <110018674+collinsmokaya28@users.noreply.github.com>
2022-10-11 19:18:10 +0300

    Update NavBar.js

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const NavBar= () => {
     return (
-        <nav>
+        <nav className="hero">
             <Link to="/home">Home</Link>
             <Link to="/about">About</Link>
             <Link to="/tourlist">TourList</Link>
