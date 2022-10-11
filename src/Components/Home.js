aa1051c469b36ad59814907f49e550a55c2efc28
Collins Mokaya <110018674+collinsmokaya28@users.noreply.github.com>
2022-10-11 19:27:38 +0300

    Update Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,7 +3,12 @@ import React from "react";
 const Home= () => {
     return (
         <div>
-            <p>This simple application was developed by Collins Mokaya. It is to be used by visitors who travel to different cities, thus it will help them to choose cities to visit based on the activities.</p>
+            <p>
+               This simple application was developed by Collins Mokaya. It is to be used by visitors who travel to different cities, thus it will help them to choose cities to visit based on the activities.
+               Once you are on the site, you can remove those tours that you are not interested with by clicking on the not interested button.
+               If you have cleared all the tours from the list ,you are able to load ,so that it fetches the data again from the Api. 
+               This apllication will have more city tours, at the moment this project is done while learning react.
+            </p>
         </div>
     )
 }
