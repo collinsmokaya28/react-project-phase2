756467e3375a439682adb84bfb680306dc486e69
Collins Mokaya <110018674+collinsmokaya28@users.noreply.github.com>
2022-10-11 19:43:30 +0300

    Update About.js

--- src/Components/About.js
+++ src/Components/About.js
@@ -7,6 +7,15 @@ const About=() => {
             <p>
                 The application gives samples of different cities and the interesting activities you can engage in once there. 
                 The developer will keep advancing according so as to include all the cities and their activities.
+                At the momemt the application is providing a sample of six cities and the fun activities you can do.
+                The developer will update the data and upload it to the Api inorder to have a variety of cities to choose from.
+            </p>
+            <h2>About the Developer</h2>
+            <p>
+                The developer currently is a student at Moring BootCamp doing Software Engineering Course on remote basis.
+                He has gained skill in software engineering, more in Frontend development and how to manipulate it and connect it to the server.
+                He has experience in HTML, CSS, Vanilla JavaScript,working with git and React. He is also an experience Data Scientist with experience in 
+                R language, Python, and SQL.
             </p>
         </div>
     )
